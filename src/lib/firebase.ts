b5ff6668790c6b1b1e03a1dfefd03feecb335e72
Ankit Sharma <ankitsharma442423@gmail.com>
2025-09-09 06:54:12 +0000

    NEXT_PUBLIC_FIREBASE_PROJECT_ID=recipeai-n2e5e NEXT_PUBLIC_FIREBASE_APP_

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "recipeai-n2e5e",
-  appId: "1:620252393384:web:9e3dad8f877eb99049b79d",
-  storageBucket: "recipeai-n2e5e.firebasestorage.app",
-  apiKey: "AIzaSyB_OFmNZBI6OUy5tHXosXmC-_5bbu83cVo",
-  authDomain: "recipeai-n2e5e.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "620252393384"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -19,3 +18,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { app, auth, db };
+
