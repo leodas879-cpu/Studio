bfbb57c88291133630d7c4aa970c1d161d728110
Ankit Sharma <ankitsharma442423@gmail.com>
2025-09-09 11:21:35 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
+
 import type {NextConfig} from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname),
-  },
+  outputFileTracingRoot: path.join(__dirname),
   typescript: {
     ignoreBuildErrors: true,
   },
